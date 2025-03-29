35d5053822bd3ca6c54a3e725492264627d3ea28
𝐁𝐀𝐎 𝐁𝐈𝐍𝐇 <114424822+baopingsheng@users.noreply.github.com>
2025-03-29 14:42:20 +0700

    Update poke_auto.js

--- poke_auto.js
+++ poke_auto.js
@@ -14,7 +14,7 @@
 
     // Configuration
     const targetSelector = '.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x87ps6o.x1lku1pv.x1a2a7pz.x9f619.x3nfvp2.xdt5ytf.xl56j7k.x1n2onr6.xh8yej3>.x1ja2u2z.x78zum5.x2lah0s.x1n2onr6.xl56j7k.x6s0dn4.xozqiw3.x1q0g3np.xi112ho.x17zwfj4.x585lrc.x1403ito.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.xn6708d.x1ye3gou.xtvsq51.x1r1pt67';
-    const clickDelay = 3000; // 3 seconds delay after clicking
+    const clickDelay = 5000; // 5 seconds delay after clicking
     const scrollAmount = 800; // Pixels to scroll each time
     const scrollInterval = 2000; // Time between scrolls (2 seconds)
     const maxScrolls = 20; // Maximum number of scroll operations
